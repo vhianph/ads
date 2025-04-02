bf4d7a59520e278339a3b43d9d66e8cdca2c0991
Le Quang Vinh <lqv140541@gmail.com>
2025-04-02 16:31:16 +0700

    a

--- q1_LQV.c
+++ q1_LQV.c
@@ -2,7 +2,7 @@
 //supose an array of size >= 6 is given. it value must be initialized by integers of your choice. 
 //arrage this array and the output array so that perfect square numbers are arranged first and then non perfect square numbers
 //Note: perfect square numbers are natural numbers whose square root is also natural
-//write the function  to check a number is a perfect square (do not use math.h library function)
+//write the function  to check a number is a perfect square (do not use math.h)
 // write the function to re-arrange perfect square numbers position in the array
 //write the function to display an input array
 int print_array(int arr[], int size)
---
+++ q2_LQV.c
@@ -0,0 +1,6 @@
+// Implement a date data structure to malnipulate time
+// A structure to store month(possible values [1:12]) and day[1:31]
+// A function initialize a date structure with a given month and day
+// A function increase a date variable by one day 
+// A function to calculate the difference between two dates
+//write main function to test the above functions
