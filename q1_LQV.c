766509ddc5c3a512ff3fe21e593d142e9f91512f
Le Quang Vinh <lqv140541@gmail.com>
2025-04-02 16:22:22 +0700

    a

---
+++ q1_LQV.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//supose an array of size >= 6 is given. it value must be initialized by integers of your choice. 
+//arrage this array and the output array so that perfect square numbers are arranged first and then non perfect square numbers
+//Note: perfect square numbers are natural numbers whose square root is also natural
+//write the function  to check a number is a perfect square
+// write the function to re-arrange perfect square numbers position in the array
+//write the function to display an input array
+int print_array(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        
+    }
+}
+//write the main function to test all the above functions.
+//Calculate the complexity of algo
+
+//O(sqrt(n))
